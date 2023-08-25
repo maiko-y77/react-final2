2c8a6551cd760d88915607f6a908b365f22f613b
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-08-25 14:23:47 -0700

    inititial commit for finalprj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { addPost, deletePost } from "./features/Posts"
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
+  const postList = useSelector((state) => state.posts.value);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(
+      addPost(
+        {
+          id: postList.length,
+          name: name,
+          content: content,
+        }
+      ));
+
+    setName("");
+    setContent("");
+  };
+  return <div className="App">
+    <div>
+      <h1>Hey, What do you want to post?</h1>
     </div>
-  );
+    <div className='addPost'>
+      <input
+        type="text"
+        placeholder='Your Name'
+        onChange={(e) => setName(e.target.value)}
+        value={name} />
+      <input
+        type="text"
+        placeholder='Comment'
+        onChange={(e) => setContent(e.target.value)}
+        value={content} />
+      <button onClick={() => handleClick()}>POST</button>
+      <hr />
+    </div>
+    <div className='displayPosts'>
+      {postList.map((post) => (
+        <div key={post.id} className="post">
+          <h1 className='postName'>{post.name}</h1>
+          <h1 className='postContent'>{post.content}</h1>
+          <button onClick={() => dispatch(deletePost({ id: post.id }))}>
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>;
 }
 
 export default App;
---
+++ src/DummyData.js
@@ -0,0 +1,24 @@
+export const PostsData = [
+    {
+        id: 1,
+        name: "John Doe",
+        content: "This is the first piece of content."
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        content: "Here's some content for the second entry."
+    },
+    {
+        id: 3,
+        name: "Alex Johnson",
+        content: "This is the third content entry."
+    },
+    {
+        id: 4,
+        name: "Emily Brown",
+        content: "Here's the fourth set of content data."
+    }
+];
+
+//Thanks ChatGPT!!
